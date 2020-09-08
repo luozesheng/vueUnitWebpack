56fee8fba2e929b8c0743032377770e6c91d653b
zesheng_luo <zesheng_luo@foxitsoftware.cn>
2020-09-08 11:46:40 +0800

    111

--- src/store/index.js
+++ src/store/index.js
@@ -15,7 +15,7 @@ let mutations = {
 };
 let actions = {
     getCommonMenuList({ commit }, payload) {
-        console.log("11111111111");
+        console.log("111111111111212");
         request.getCommonMenu(res => {
             commit(types.COMMON_MENULIST, res);
         }, payload.params);
