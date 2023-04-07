f5a22c450cb16b27d22f8d1b0c5c41653ca3d8fd
mac <13625022820@163.com>
2023-04-07 16:29:42 +0800

    添加

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,45 +2,37 @@
 <template>
   <div class="wrapper">
     主页aaaaaaa
-    <DatePicker @change="changeDate"/>
   </div>
 </template>
 <!--组件逻辑-->
 <script>
-  import { DatePicker } from 'ant-design-vue';
-  export default {
-    name: "Home",
-    data() {
-      return {
-        msg: 'Hello,Webpack',
-        userName: "玛丽亚"
-      }
-    },
-    components:{
-      DatePicker
-    },
-    methods:{
-      changeDate(v){
-        console.log('aaaa', v);
-      },
-    },
-  }
-</script>
-<style lang="less" scoped>
-  .wrapper{
-    width: 100%;
-    height: 50px;
-    line-height: 50px;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    .xheader_title{
-      width: 50%;
-      height: 50px;
-      line-height: 50px;
-      color: #999999;
-      font-size: 16px;
-      font-weight: 550;
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      msg: 'Hello,Webpack',
+      userName: "玛丽亚"
     }
-  }
+  },
+  components: {
+    DatePicker
+  },
+  methods: {
+    changeDate(v) {
+      console.log('aaaa', v);
+    },
+  },
+}
+</script>
+<style scoped>
+.wrapper {
+  width: 100%;
+  height: 50px;
+  line-height: 50px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+}
 </style>
--- webpack.config.js
+++ webpack.config.js
@@ -58,8 +58,28 @@ let config = {
                         //     singleton: 'true' // 将所有的 style 标签合成一个
                         // }
                     },
-                    {loader: 'css-loader'},
-                    {loader: 'less-loader'},
+                    {
+                        test: /\.s(c|a)ss$/,
+                        use: [
+                          'vue-style-loader',
+                          'css-loader',
+                          {
+                            loader: 'sass-loader',
+                            // Requires sass-loader@^7.0.0
+                            options: {
+                              implementation: require('sass'),
+                              indentedSyntax: true // optional
+                            },
+                            // Requires >= sass-loader@^8.0.0
+                            options: {
+                              implementation: require('sass'),
+                              sassOptions: {
+                                indentedSyntax: true // optional
+                              },
+                            },
+                          },
+                        ],
+                    },
                     {
                         loader: 'postcss-loader',
                         options:{
